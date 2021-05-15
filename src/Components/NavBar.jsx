dd6702b55504246cd13ba01b7ff3641e6b9e22cd
Sunil <sunil@Sunils-MacBook-Air.local>
2021-05-15 15:01:09 +0530

    Basic setup done

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,24 +4,26 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBuilding } from '@fortawesome/free-solid-svg-icons';
 
 class NavBar extends React.Component {
-    
+
     render() {
         return (
             <>
-                <Navbar bg="dark" variant="dark" sticky="top">
-                    <Navbar.Brand href="#home">
-                        <FontAwesomeIcon icon={faBuilding} />
-                        <span class="ml-3">  COVID-19 INFORMATION </span>
-                    </Navbar.Brand>
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="row-reverse">
-                            <Nav.Link eventKey={2} href="/cases">
+                <div className="container-fluid mt-2">
+                    <div className="row">
+                        <div className="col-12 col-md-6">
+                            <FontAwesomeIcon icon={faBuilding} />
+                            <span class="ml-3">  COVID-19 INFORMATION </span>
+                        </div>
+                        <div className="col-12 col-md-6 text-right">
+                            <a href="/cases" className="mr-3">
                                 Cases
-                            </Nav.Link>
-                            <Nav.Link href="/home">Hospitals</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
+                        </a>
+                            <a href="/home">
+                                Hospitals
+                        </a>
+                        </div>
+                    </div>
+                </div>
             </>
         );
     }
